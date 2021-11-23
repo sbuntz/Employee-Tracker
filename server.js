af3dfe1734667d65190426036a8056f8c89c4061
Stefanie Buntz <stbuntz@gmail.com>
2021-11-23 20:03:39 +1030

    sql files

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'Notitia@123',
+    database: 'company'
+  },
+  console.log(`Connected to the company database.`)
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
